ef1d5067585c05bfa96b97cd96c14c59e19e9fe4
Paulo César <khaos858@gmail.com>
2023-03-23 01:03:28 -0300

    Teste

---
+++ Exerc2/Program.cs
@@ -0,0 +1,16 @@
+﻿int Fibonacci = 0, fb1 = 0, fb2 = 1;
+
+Console.Write("Insira um número inteiro para verificar se ele pertence ou não à sequência de Fibonacci: ");
+int num = int.Parse(Console.ReadLine());
+
+while(Fibonacci != num && Fibonacci < num){
+    Fibonacci = fb1 + fb2;
+    fb1 = fb2;
+    fb2 = Fibonacci;
+}
+
+if(Fibonacci == num){
+    Console.WriteLine("O número informado pertence à sequência de Fibonacci!");
+}else{
+    Console.WriteLine("O número informado não pertence à sequência de Fibonacci.");
+}
---
+++ Exerc5/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Insira a palavra para que invertamos os caracteres dela: ");
+string palavra = Console.ReadLine();
+palavra.ToArray();
+string inverso = "";
+for (int i = palavra.Length - 1; i >= 0; i--){
+    inverso += palavra[i].ToString();
+}
+
+Console.WriteLine(inverso);
